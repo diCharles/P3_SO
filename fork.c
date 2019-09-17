aec02b70d4efe405a463835bd51b7c133c25ed74
diCharles <43318661+diCharles@users.noreply.github.com>
2019-09-17 11:08:59 -0500

    fork is exercise 1.4 p3

---
+++ fork.c
@@ -0,0 +1,12 @@
+// http://users.cs.cf.ac.uk/Dave.Marshall/C/node27.html
+
+
+
+// shemget get a shared memory segment
+
+// shemat attach/dettach a shared memory segment
+int main()
+{
+
+  return ( 0);
+}
