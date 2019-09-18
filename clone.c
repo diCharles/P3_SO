d61a7944131363e2747154368e84ae41504e7606
diCharles <ie709758@iteso.mx>
2019-09-17 20:26:49 -0600

    significant progress in clone

--- clone.c
+++ clone.c
@@ -16,9 +16,11 @@ also receives a value througth   pointer
 #include <sched.h>
 #include <sys/wait.h>
 
-#define N_CLONES 4U // NUMBER OF CLONES TO BE CREATED
 
-int g_shared_var = 0;
+#define N_CLONES 4U // NUMBER OF CLONES TO BE CREATED
+#define ITERACIONES 2000000000
+
+long double pi_acumm[N_CLONES] ={ 0} ;
 
 // thread that differ from parent in execution
 
@@ -26,38 +28,46 @@ int child_fn(void * arg)
 {
 		int val = * ((int *) arg ) ;
 		printf("clonated child  received %d \n", val);
-		g_shared_var = 5 ;
+		pi_acumm[val] +=1 ;
+		exit(0);
 }	
 
 int main()
 {
-	// allocate mem for clone thread
-	void * child_stack = malloc(1024*1024);
-
-		// signal any error in allocation
-		if ( NULL== child_stack)
-		{	
-			printf("unable to allocate mem for child \n");		
-		}
-		else
+	void * child_stack[N_CLONES] = {NULL};
+	pid_t pid_chids[N_CLONES]= {0};
+	
+	int clone_maker = 0 ;
+	for(clone_maker = 0 ; clone_maker<N_CLONES; clone_maker++)
+	{
+		// allocate mem for clone thread
+		
+		child_stack[clone_maker]= malloc(1024*1024);
+		//creating child with a given value
+		int val_to_child = 1;
+		pid_chids[clone_maker] = clone( child_fn , child_stack[clone_maker] + (1024 * 1024) , SIGCHLD | CLONE_VM , (void * ) (&val_to_child)); 
+		
+		/** check if any of previous proccess went wrong*/
+		if(  (NULL== child_stack[clone_maker])  ||  (0 > pid_chids[clone_maker])   )
 		{
-			//passing value to child
-			int val_to_child = 4;
-			
-			int pid_child = clone( child_fn , child_stack + (1024 * 1024) , SIGCHLD  | CLONE_VM , (void * ) (&val_to_child)); 
-			if( 0 > pid_child)
-			{
-				printf("unable to clone a child \n");
-			}	
-			else
-			{
-				
-				//able to clone a child
-				wait(NULL) ; // better wait his pid
-				printf("child wrote %d \n to shared var",g_shared_var);
-				free(child_stack);
-			}
+			printf(" fatal error could not create all child clones\n");
+			// free clone child mem
+			return 0;
 		}
+		
+	}
+	
+	// wait until  clone childs finished
+	for(clone_maker = 0 ; clone_maker<N_CLONES; clone_maker++)
+	{
+		
+		wait(NULL);
+		//free clone child mem
+		free(child_stack[clone_maker]);
+	}
+	long double pi  = pi_acumm[0] +  pi_acumm[1] + pi_acumm[2] + pi_acumm[3] ;
+	printf("PI con %d iteraciones: %Lf\n", ITERACIONES, (pi*4));
+	
 
 	return 0 ; 
 }
